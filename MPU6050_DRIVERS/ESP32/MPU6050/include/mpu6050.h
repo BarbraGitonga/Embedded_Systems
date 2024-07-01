070786bac0482bf5547809ffae8233be66c8470a
Barbra-Gitonga <gitongabarbra@gmail.com>
2024-07-02 00:00:40 +0300

    registers

---
+++ MPU6050_DRIVERS/ESP32/MPU6050/include/mpu6050.h
@@ -0,0 +1,52 @@
+/**
+ * @file mpu6050.h
+ * @author Barbra Gitonga (gitongabarbra@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-06-15
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#ifndef MPU6050_I2C_DRIVER_H
+#define MPU6050_I2C_DRIVER_H
+
+#define SELF_TEST_X         0x0D        
+#define SELF_TEST_Y         0x0E
+#define SELF_TEST_Z         0x0F
+#define SELF_TEST_A         0x10
+
+#define SMPLRT_DIV          0x19
+
+#define CONFIG              0x1A
+#define GYRO_CONFIG         0x1B
+#define ACCEL_CONFIG        0x1C
+#define FIFO_EN             0x23
+#define I2C_MST_CTRL        0x24
+
+#define ACCEL_XOUT_H        0x3B
+#define ACCEL_XOUT_L        0x3C
+#define ACCEL_YOUT_H        0x3D
+#define ACCEL_YOUT_L        0x3E
+#define ACCEL_ZOUT_H        0x3F
+#define ACCEL_ZOUT_L        0x40
+
+#define TEMP_OUT_H          0x41
+#define TEMP_OUT_L          0x42
+
+#define GYRO_XOUT_H         0x43
+#define GYRO_XOUT_L         0x44
+#define GYRO_YOUT_H         0x45
+#define GYRO_YOUT_L         0x46
+#define GYRO_ZOUT_H         0x47
+#define GYRO_ZOUT_L         0x48
+
+#define PWR_MGMT_1          0x6B
+#define FIFO_COUNTH         0x72
+#define FIFO_COUNTL         0x73
+#define FIFO_R_W            0x74
+
+#define WHO_AM_I            0x75
+
+#endif
